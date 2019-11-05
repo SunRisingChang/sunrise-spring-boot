861243c53a0036a60be91430017ea460f93ed2bb
Sun Rising <275050943@qq.com>
2019-11-05 19:57:47 +0800

    添加跨域支持

--- spring-boot-sunrise-core/src/main/java/com/sunrise/core/config/webmvc/SpringWebConfig.java
+++ spring-boot-sunrise-core/src/main/java/com/sunrise/core/config/webmvc/SpringWebConfig.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -36,6 +41,40 @@ public class SpringWebConfig implements WebMvcConfigurer {
 
 	@Autowired
 	private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
+
+	/**
+	 * 配置跨域
+	 * 
+	 * @author Sun_Rising
+	 * @date 2018.12.27 01:51:06
+	 * 
+	 */
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*"); // 允许任何域名使用
+		corsConfiguration.addAllowedHeader("*"); // 允许任何头
+		corsConfiguration.addAllowedMethod("*"); // 允许任何方法
+		corsConfiguration.setMaxAge(18000L); // 预检请求的有效期，单位为秒。
+		corsConfiguration.setAllowCredentials(true);// 是否支持安全证书(必需参数)
+		return corsConfiguration;
+	}
+
+	/**
+	 * 使用跨域
+	 * 
+	 * @author Sun_Rising
+	 * @date 2018.12.27 01:51:06
+	 * 
+	 */
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig());
+		FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<CorsFilter>(
+				new CorsFilter(source));
+		filterRegistrationBean.setOrder(0);// 设置监听器的优先级
+		return filterRegistrationBean;
+	}
 
 	/**
 	 * 添加FastjsonHTTP信息转换器
@@ -76,7 +115,8 @@ public class SpringWebConfig implements WebMvcConfigurer {
 				break;
 			}
 		}
-		PostEntityHandlerMethodArgumentResolver postEntityHandlerMethodArgumentResolver = new PostEntityHandlerMethodArgumentResolver(requestResponseBodyMethodProcessor, servletModelAttributeMethodProcessor);
+		PostEntityHandlerMethodArgumentResolver postEntityHandlerMethodArgumentResolver = new PostEntityHandlerMethodArgumentResolver(
+				requestResponseBodyMethodProcessor, servletModelAttributeMethodProcessor);
 		List<HandlerMethodArgumentResolver> newResolvers = new ArrayList<>(argumentResolvers.size() + 1);
 		newResolvers.add(postEntityHandlerMethodArgumentResolver);
 		newResolvers.addAll(argumentResolvers);
